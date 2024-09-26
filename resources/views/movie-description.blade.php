afd10e4eb953f537482d216024a0dfc92f70cd26
Brandon Wilson <brandonwilson@MacBook-Pro-193.local>
2024-09-26 19:21:05 -0400

    updating description page

--- resources/views/movie-description.blade.php
+++ resources/views/movie-description.blade.php
@@ -1,29 +1,61 @@
-@props(['movieDetails'])
+<?php
+//   dd($movie)
+    // movie genres from tmdb 
+ $genres = [
+    28 => 'Action',
+    12 => 'Adventure',
+    16 => 'Animation',
+    35 => 'Comedy',
+    80 => 'Crime',
+    99 => 'Documentary',
+    18 => 'Drama',
+    10751 => 'Family',
+    14 => 'Fantasy',
+    36 => 'History',
+    27 => 'Horror',
+    10402 => 'Music',
+    9648 => 'Mystery',
+    10749 => 'Romance',
+    878 => 'Science Fiction',
+    10770 => 'TV Movie',
+    53 => 'Thriller',
+    10752 => 'War',
+    37 => 'Western',
+];
+?>
 
 <x-layout>
     <div class="mt-14"></div>
     <div class="mx-24 flex">
-        <div class="border border-white border-opacity-25 rounded-md py-60 px-40">Movie Image</div>
+        <div class="border border-white border-opacity-25 rounded-md py-60 px-40">
+            <img src="https://image.tmdb.org/t/p/w500{{ $movie['poster_path'] }}" alt="">
+        </div>
+        
         <div class="pl-12"></div>
         <div>
             <div class="flex flex-row justify-between">
                 <div>
                     <h1 class="text-mega text-white font-serif"> {{ $movie['title'] }}</h1>
-                    <h3 class="text-body text-white text-opacity-50 font-sans">Movie Release Date | Rating | Time</h3>
+                    <!-- movie data - movie rating (pg/r) - how long movie is-->
+                    <h3 class="text-body text-white text-opacity-50 font-sans">{{ $movie['release_date'] }} | {{ $movie['popularity'] }} | Time</h3>
                 </div>
                 <div class="flex">
-                    <div class="border border-white border-opacity-25 text-white p-12 rounded-md">star ratings</div>
+                    @php
+                        $percent = round($movie['vote_average'] * 10)
+                    @endphp
+                    <div class="border border-white border-opacity-25 text-white p-12 rounded-md"> {{ $percent }}</div>
                     <div class="mr-6"></div>
                     <div class="border border-white border-opacity-25 text-white p-12 rounded-md"># of watchlists</div>
                 </div>
             </div>
             <div class="mt-6"></div>
-            <p class="text-white text-sm text-opacity-50">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus nulla vel accusamus quam magnam dolorem dolore nemo, dignissimos soluta expedita harum, aperiam laudantium pariatur unde blanditiis labore architecto ipsam natus, minus modi atque! Minus non ducimus quas veniam optio magni soluta perspiciatis ipsum voluptas. Harum amet consequatur voluptatum illo est! Corporis saepe nobis itaque, aperiam nulla blanditiis architecto quaerat aliquam obcaecati, quo quasi reiciendis inventore natus vitae, dicta modi veritatis. Non nam minus distinctio, ipsa fugiat, consequatur rerum quos optio fuga excepturi assumenda voluptatem natus provident eos cum. Ipsum facere debitis laboriosam, quaerat delectus molestiae qui cumque cum mollitia quo.</p>
+            <p class="text-white text-sm text-opacity-50"> {{ $movie['overview'] }}</p>
             <div class="flex flex-row mt-6">
-                <x-genre-tag title="Sci-Fi"></x-genre-tag>
-                <x-genre-tag title="Horror"></x-genre-tag>
-                <x-genre-tag title="Thriller"></x-genre-tag>
-                <x-genre-tag title="Drama"></x-genre-tag>
+                @foreach ($movie['genre_ids'] as $genre_id)
+                    @if (isset($genres[$genre_id]))
+                        <x-genre-tag title="{{ $genres[$genre_id] }}"></x-genre-tag>
+                    @endif
+                @endforeach
             </div>
             <div class="flex flex-row mt-6"></div>
         </div>
