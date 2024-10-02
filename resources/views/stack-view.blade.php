1121889d0de026a8e4c9b2a0b54022f82ec3ab78
“Aiden <adnrdmnd@gmail.com>
2024-10-02 10:06:52 -0400

    tiny visual tweaks for stack creation

--- resources/views/stack-view.blade.php
+++ resources/views/stack-view.blade.php
@@ -7,40 +7,29 @@
     </div>
     @endif
 
-    <div class="flex justify-between items-end mx-32">
+    <div class="flex justify-between items-end mx-40">
         <div class="flex flex-col items-start">
             <h1 class="text-blue text-mega font-serif mt-12 text-center max-w-5xl">{{ $stackTitle }}</h1>
             <p class="text-white text-center text-body">{{ $stackDescription }}</p>
         </div>
 
-        <div class="flex space-x-4 mt-6">
-            <button class="px-3 p-2 text-white">Edit Stack</button>
-            <form action="/stack?id={{ request('id') }}" method="POST" class="inline">
+        <div class="flex flex-col space-y-6 mt-6 items-end">
+            <button><img src="images/edit.png" alt="" class="w-8"></button>
+            <form action="/stack?id={{ request('id') }}" method="POST" class="h-8">
                 @csrf
                 @method('DELETE')
-                <button type="submit" class="px-3 p-2 text-red-600">
-                    Delete Stack
-                </button>
+                <button type="submit"><img src="images/delete.png" alt="Delete" class="w-8"></button>
             </form>
-
         </div>
+
     </div>
 
-    <div class="mt-24"></div>
+    <x-hr class="mt-12" />
 
     <div class="flex space-x-8 justify-center">
         <x-stack-movie-poster img="images/movie_posters/parasite.jpg" name="Parasite"/>
         <x-stack-movie-poster img="images/movie_posters/batman.png" name="The Batman"/>
         <x-stack-movie-poster img="images/movie_posters/Lobster.jpg" name="The Lobster"/>
-        <x-stack-movie-poster img="images/movie_posters/opp.avif" name="Oppenheimer"/>
-        <x-stack-movie-poster img="images/movie_posters/joker.jpeg" name="The Joker"/>
-    </div>
-
-    <div class="flex space-x-8 justify-center">
-        <x-stack-movie-poster img="images/movie_posters/parasite.jpg" name="Parasite"/>
-        <x-stack-movie-poster img="images/movie_posters/batman.png" name="The Batman"/>
-        <x-stack-movie-poster img="images/movie_posters/Lobster.jpg" name="The Lobster"/>
-        <x-stack-movie-poster img="images/movie_posters/opp.avif" name="Oppenheimer"/>
         <x-stack-movie-poster img="images/movie_posters/joker.jpeg" name="The Joker"/>
     </div>
 </x-layout>
