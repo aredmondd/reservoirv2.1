638567af4673eaadf9d9e48a6e2dfb150e6188dc
“Aiden <adnrdmnd@gmail.com>
2024-10-23 20:18:34 -0400

    pagination for watchlist & history

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,8 +21,6 @@
             </div>
         </form>
 
-
-
         <div class="flex items-center pb-2 space-x-4">
             <button class="mt-2 inline-flex items-center px-4 py-2 {{ request()->input('view') == 'watchlist' || !request()->input('view') ? 'bg-blue' : 'bg-white bg-opacity-75'}} rounded-md font-semibold text-sm text-midnight uppercase tracking-widest hover:bg-blue focus:outline focus:outline-2 transition ease-in-out duration-150"><a href="/dashboard?view=watchlist">Watchlist</a></button>
             <button class="mt-2 inline-flex items-center px-4 py-2 {{ request()->input('view') == 'history' ? 'bg-blue' : 'bg-white bg-opacity-75'}} rounded-md font-semibold text-sm text-midnight uppercase tracking-widest hover:bg-blue focus:outline focus:outline-2 transition ease-in-out duration-150"><a href="/dashboard?view=history">History</a></button>
@@ -39,12 +37,17 @@
 
     <hr class='border-white border-opacity-25 mx-40 my-3'>
 
-    @if ($list == null)
+    @if ($list->isEmpty())
         <x-empty />
     @else
         @foreach ($list as $content)
             <x-content-row :content='$content'/>
         @endforeach
+
+        <!-- Pagination Links -->
+        <div class="mt-6">
+            {{ $list->appends(request()->query())->links('vendor.pagination.tailwind') }}
+        </div>
     @endif
 
     <div class="mb-24"></div>
