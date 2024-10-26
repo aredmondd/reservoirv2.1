51bcb39d44d1315ee8a0da7e0a320f762a2ea6ba
“Aiden <adnrdmnd@gmail.com>
2024-10-26 15:25:22 -0400

    fix brandon bug :)))))

--- resources/views/components/add-to-stack-button.blade.php
+++ resources/views/components/add-to-stack-button.blade.php
@@ -53,11 +53,3 @@ $userStacks = Stack::where('user_id', $user->id)->get();
         </form>
     </div>
 </x-modal>
-
-<!-- <form action='/addToStack?id={{$id}}&content_type={{$flag}}' method="POST">
-    @csrf
-
-    <button type="submit" 
-            class="border border-blue rounded-full text-sm text-blue p-2 px-3 hover:cursor-pointer"
-            onclick="showMessage()" >Add to Stack</button>
-</form>
