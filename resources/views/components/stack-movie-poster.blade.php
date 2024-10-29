bb49e289c0e458ae4bca0dcd258ae03345d03c0a
“Aiden <adnrdmnd@gmail.com>
2024-10-29 19:05:39 -0400

    stack view refactor

--- resources/views/components/stack-movie-poster.blade.php
+++ resources/views/components/stack-movie-poster.blade.php
@@ -27,15 +27,14 @@ elseif ($contentType == 'tv') {
 ?>
 
 <div class="flex flex-col items-center">
-    <img src="{{ $posterPath ? 'https://image.tmdb.org/t/p/w500' . $posterPath : asset('images/no-movie-poster.jpg') }}" class="rounded-md w-56 mb-2" alt="{{ $name }} movie poster">
-    <div class="flex justify-between items-center w-full max-w-56">
-        <p class="text-white text-body font-sans font-bold">{{ $name }}</p>
-        <form action="/stack-content?stackID={{ $stackID }}&contentID={{ $item['id'] }}" method="POST" class="h-8">
-            @csrf
-            @method('DELETE')
-            <button type="submit">
-                <img src="images/delete.png" alt="Delete" class="w-6">
-            </button>
-        </form>
-    </div>
+    <a href="{{ route('movie-description', ['movie' => $item['id'], 'flag' => $item['contentType']]) }}">
+        <img src="{{ $posterPath ? 'https://image.tmdb.org/t/p/w500' . $posterPath : asset('images/no-movie-poster.jpg') }}" class="rounded-md w-56 mb-2 hover:bg-blue" alt="{{ $name }} movie poster">
+    </a>
+    <form action="/stack-content?stackID={{ $stackID }}&contentID={{ $item['id'] }}" method="POST" class="h-8 delete-button hidden">
+        @csrf
+        @method('DELETE')
+        <button type="submit">
+            <img src="images/delete.png" alt="Delete" class="w-6">
+        </button>
+    </form>
 </div>
--- resources/views/stack-view.blade.php
+++ resources/views/stack-view.blade.php
@@ -10,25 +10,34 @@
         </div>
 
         <div class="flex flex-col space-y-6 mt-6 items-end">
-            <button><img src="images/edit.png" alt="" class="w-8"></button>
+            <span id="editButton" class="material-symbols-outlined text-white text-title cursor-pointer">edit</span>
             <form action="/stack?id={{ request('id') }}" method="POST" class="h-8">
                 @csrf
                 @method('DELETE')
                 <button type="submit"><img src="images/delete.png" alt="Delete" class="w-8"></button>
             </form>
         </div>
-
     </div>
 
     @if ($content == null)
         <div class="text-white text-center mt-24">Search for some movies to add to your stack in "Fill your Reservoir"</div>
     @else
         <div class="container mx-auto p-8 mt-12 mx-40">
-            <div class="grid grid-cols-4 gap-6">
+            <div class="grid grid-cols-5 gap-6">
                 @foreach($content as $item)
                     <x-stack-movie-poster :item="$item" :stackID="request('id')" />
                 @endforeach
             </div>
         </div>
     @endif
+
+    <script>
+        document.getElementById('editButton').addEventListener('click', function() {
+            const deleteButtons = document.querySelectorAll('.delete-button');
+            deleteButtons.forEach(button => {
+                button.classList.toggle('hidden');
+            });
+        });
+    </script>
+
 </x-layout>
