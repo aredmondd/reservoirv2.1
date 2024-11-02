70ae0ebef8b6aba6353bc0ab79d57320635c1e58
“Aiden <adnrdmnd@gmail.com>
2024-11-02 15:22:41 -0400

    displaying numbers of profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+
+$numWatchlisted = count(Auth::user()->watchlist->watchlist);
+$numWatched = count(Auth::user()->history->history);
+
+?>
+
 <x-layout>
 
     <h1 class="text-white text-center text-mega font-serif">My profile</h1>
+
+    <h1 class="text-white"># of items watchlisted: {{ $numWatchlisted }}</h1>
+    <h1 class="text-white"># of items watched: {{ $numWatched }}</h1>
 
 </x-layout>
