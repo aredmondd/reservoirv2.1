7c1be933d543766eb32b0bc47fe6ce3c8f2b6826
“Aiden <adnrdmnd@gmail.com>
2024-11-13 14:29:52 -0500

    profile fixes + begin friend tweaks

--- resources/views/user-profile/history.blade.php
+++ resources/views/user-profile/history.blade.php
@@ -1,5 +1,12 @@
 <x-profile-wrapper :user='$user'>
-    <x-watchlist-header />
+    <div class="grid grid-cols-6 mx-40">
+        <div class="col-span-5 grid grid-cols-6 text-white">
+            <p>date added</p>
+            <p class="col-span-2">content</p>
+            <p id ='released'>released</p>
+            <p id ='length'>length</p>
+        </div>
+    </div>
 
     <hr class='border-white border-opacity-25 mx-40 my-3'>
 
