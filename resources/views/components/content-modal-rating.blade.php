d015ed0a3b76f2f4a3071367d02ab9467c4b1a6a
“Aiden <adnrdmnd@gmail.com>
2024-11-14 14:19:10 -0500

    star submitting works

--- resources/views/components/content-modal-rating.blade.php
+++ resources/views/components/content-modal-rating.blade.php
@@ -4,26 +4,18 @@
 
 <x-modal name="add-history-rating-dashboard-{{ $id }}" :show="$errors->isNotEmpty()" focusable>
     <div class="bg-midnight p-8 rounded-md shadow-lg max-w-md mx-auto" x-data="{ stars: null, hovered: 0 }">
-        <h2 class="text-title font-serif text-center text-blue mb-4">What did you think of {{ $name }}? </h2>
+        <h2 class="text-title font-serif text-center text-blue mb-4">Rate {{ $name }}</h2>
 
         <div class="flex justify-center mb-6">
-            <label class="mr-4 text-lg">Rating:</label>
             <div class="flex">
                 <!-- Stars for Rating -->
-                <span x-on:mouseover="hovered = 1" x-on:click="stars = 1" x-bind:class="hovered >= 1 || stars >= 1 ? 'text-amber-400' : 'text-white text-opacity-25'" class="text-title material-symbols-outlined cursor-pointer">star</span>
-                <span x-on:mouseover="hovered = 2" x-on:click="stars = 2" x-bind:class="hovered >= 2 || stars >= 2 ? 'text-amber-400' : 'text-white text-opacity-25'" class="text-title material-symbols-outlined cursor-pointer">star</span>
-                <span x-on:mouseover="hovered = 3" x-on:click="stars = 3" x-bind:class="hovered >= 3 || stars >= 3 ? 'text-amber-400' : 'text-white text-opacity-25'" class="text-title material-symbols-outlined cursor-pointer">star</span>
-                <span x-on:mouseover="hovered = 4" x-on:click="stars = 4" x-bind:class="hovered >= 4 || stars >= 4 ? 'text-amber-400' : 'text-white text-opacity-25'" class="text-title material-symbols-outlined cursor-pointer">star</span>
-                <span x-on:mouseover="hovered = 5" x-on:click="stars = 5" x-bind:class="hovered >= 5 || stars >= 5 ? 'text-amber-400' : 'text-white text-opacity-25'" class="text-title material-symbols-outlined cursor-pointer">star</span>
+                <button type="submit" x-on:click="document.getElementById('rating-form-dashboard-{{ $id }}').action += '&rating=' + 1" x-on:mouseover="hovered = 1" x-bind:class="hovered >= 1 || stars >= 1 ? 'text-amber-400' : 'text-white text-opacity-25'" class="text-mega material-symbols-outlined cursor-pointer">star</button>
+                <button type="submit" x-on:click="document.getElementById('rating-form-dashboard-{{ $id }}').action += '&rating=' + 2" x-on:mouseover="hovered = 2" x-bind:class="hovered >= 2 || stars >= 2 ? 'text-amber-400' : 'text-white text-opacity-25'" class="text-mega material-symbols-outlined cursor-pointer">star</span>
+                <button type="submit" x-on:click="document.getElementById('rating-form-dashboard-{{ $id }}').action += '&rating=' + 3" x-on:mouseover="hovered = 3" x-bind:class="hovered >= 3 || stars >= 3 ? 'text-amber-400' : 'text-white text-opacity-25'" class="text-mega material-symbols-outlined cursor-pointer">star</span>
+                <button type="submit" x-on:click="document.getElementById('rating-form-dashboard-{{ $id }}').action += '&rating=' + 4" x-on:mouseover="hovered = 4" x-bind:class="hovered >= 4 || stars >= 4 ? 'text-amber-400' : 'text-white text-opacity-25'" class="text-mega material-symbols-outlined cursor-pointer">star</span>
+                <button type="submit" x-on:click="document.getElementById('rating-form-dashboard-{{ $id }}').action += '&rating=' + 5" x-on:mouseover="hovered = 5" x-bind:class="hovered >= 5 || stars >= 5 ? 'text-amber-400' : 'text-white text-opacity-25'" class="text-mega material-symbols-outlined cursor-pointer">star</span>
             </div>
         </div>
-
-        <!-- Submit Rating Button -->
-        <button type="submit" 
-                x-on:click="document.getElementById('rating-form-dashboard-{{ $id }}').action += '&rating=' + stars"
-                class="text-white bg-blue rounded-full px-4 py-2 font-medium tracking-wide hover:bg-blue-600 transition">
-            Submit
-        </button>
     </div>
 </x-modal>
 
