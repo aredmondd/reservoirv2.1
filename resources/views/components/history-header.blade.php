b58677a6f1f187fbd8061b59c115878c1b0bc409
“Aiden <adnrdmnd@gmail.com>
2024-11-16 10:06:16 -0500

    dynamic arrows

--- resources/views/components/history-header.blade.php
+++ resources/views/components/history-header.blade.php
@@ -1,11 +1,48 @@
 @props(['sortOrder'])
 
+<?php
+
+if (request()->input('sortOrder') == null || request()->input('sortOrder') == 'asc' ) {
+    $icon = '';
+} elseif (request()->input('sortOrder') == 'normal') {
+    $icon = '<span class="material-symbols-outlined text-blue">keyboard_arrow_up</span>';
+} elseif(request()->input('sortOrder') == 'desc') {
+    $icon = '<span class="material-symbols-outlined text-blue">keyboard_arrow_down</span>';
+}
+
+?>
+
 <div class="grid grid-cols-6 mx-40">
     <div class="col-span-5 grid grid-cols-6 text-white">
+    <div class="flex">
         <a href = "{{ route('display_list', ['view' => request()->input('view', 'history'), 'filterBy' => 'time', 'sortOrder' => $sortOrder]) }}">date added</a>
-        <a class="col-span-2" href = "{{ route('display_list', ['view' => request()->input('view', 'history'), 'filterBy' => 'name', 'sortOrder' => $sortOrder]) }}">content</a>
+        @if(request()->input('filterBy') == 'time')
+            {!! $icon !!}
+        @endif
+    </div>
+    <div class="flex col-span-2">
+        <a href = "{{ route('display_list', ['view' => request()->input('view', 'history'), 'filterBy' => 'name', 'sortOrder' => $sortOrder]) }}">content</a>
+        @if(request()->input('filterBy') == 'name')
+            {!! $icon !!}
+        @endif
+    </div>
+    <div class="flex">
         <a id ='released' href = "{{ route('display_list', ['view' => request()->input('view', 'history'), 'filterBy' => 'released', 'sortOrder' => $sortOrder]) }}">released</a>
+        @if(request()->input('filterBy') == 'released')
+            {!! $icon !!}
+        @endif
+    </div>
+    <div class="flex">
         <a id ='length' href = "{{ route('display_list', ['view' => request()->input('view', 'history'), 'filterBy' => 'length', 'sortOrder' => $sortOrder]) }}">length</a>
+        @if(request()->input('filterBy') == 'length')
+            {!! $icon !!}
+        @endif
+    </div>
+    <div class="flex">
         <a href = "{{ route('display_list', ['view' => request()->input('view', 'history'), 'filterBy' => 'rating', 'sortOrder' => $sortOrder]) }}">rating</a>
+        @if(request()->input('filterBy') == 'rating')
+            {!! $icon !!}
+        @endif
+    </div>
     </div>
 </div>
--- resources/views/components/watchlist-header.blade.php
+++ resources/views/components/watchlist-header.blade.php
@@ -1,10 +1,42 @@
 @props(['sortOrder'])
 
+<?php
+
+if (request()->input('sortOrder') == null || request()->input('sortOrder') == 'asc' ) {
+    $icon = '';
+} elseif (request()->input('sortOrder') == 'normal') {
+    $icon = '<span class="material-symbols-outlined text-blue">keyboard_arrow_up</span>';
+} elseif(request()->input('sortOrder') == 'desc') {
+    $icon = '<span class="material-symbols-outlined text-blue">keyboard_arrow_down</span>';
+}
+
+?>
+
 <div class="grid grid-cols-6 mx-40">
     <div class="col-span-5 grid grid-cols-6 text-white">
-        <a href = "{{ route('display_list', ['view' => request()->input('view', 'watchlist'), 'filterBy' => 'time', 'sortOrder' => $sortOrder]) }}">date added</a>
-        <a class="col-span-2" href = "{{ route('display_list', ['view' => request()->input('view', 'watchlist'), 'filterBy' => 'name', 'sortOrder' => $sortOrder]) }}">content</a>
-        <a id ='released' href = "{{ route('display_list', ['view' => request()->input('view', 'watchlist'), 'filterBy' => 'released', 'sortOrder' => $sortOrder]) }}">released</a>
-        <a id ='length' href = "{{ route('display_list', ['view' => request()->input('view', 'watchlist'), 'filterBy' => 'length', 'sortOrder' => $sortOrder]) }}">length</a>
+        <div class="flex">
+            <a href = "{{ route('display_list', ['view' => request()->input('view', 'watchlist'), 'filterBy' => 'time', 'sortOrder' => $sortOrder]) }}">date added</a>
+            @if(request()->input('filterBy') == 'time')
+                {!! $icon !!}
+            @endif
+        </div>
+        <div class="flex col-span-2">
+            <a href = "{{ route('display_list', ['view' => request()->input('view', 'watchlist'), 'filterBy' => 'name', 'sortOrder' => $sortOrder]) }}">content</a>
+            @if(request()->input('filterBy') == 'name')
+                {!! $icon !!}
+            @endif
+        </div>
+        <div class="flex">
+            <a id ='released' href = "{{ route('display_list', ['view' => request()->input('view', 'watchlist'), 'filterBy' => 'released', 'sortOrder' => $sortOrder]) }}">released</a>
+            @if(request()->input('filterBy') == 'released')
+                {!! $icon !!}
+            @endif
+        </div>
+        <div class="flex">
+            <a id ='length' href = "{{ route('display_list', ['view' => request()->input('view', 'watchlist'), 'filterBy' => 'length', 'sortOrder' => $sortOrder]) }}">length</a>
+            @if(request()->input('filterBy') == 'length')
+                {!! $icon !!}
+            @endif
+        </div>
     </div>
 </div>
